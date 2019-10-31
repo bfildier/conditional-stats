9bfea0fdcc7e033920b05dad0b35290fd6ac9123
Benjamin Fildier <bfildier.work@gmail.com>
2019-11-01 00:22:07 +0100

    add methods indexOfRank, rankID, binIndex, storeSamplePoints to class Distribution, methods in class Conditional Distribution to be added

--- modules/conditionalstats.py
+++ modules/conditionalstats.py
@@ -88,7 +88,6 @@ class Distribution(EmptyDistribution):
 
     def __init__(self,name='',bintype='linear',nbpd=10,nppb=4,nlb=50,nlr=100,\
         fill_last_decade=False):
-
         """Constructor for class Distribution.
         Arguments:
         - name: name of reference variable
@@ -101,6 +100,7 @@ class Distribution(EmptyDistribution):
         EmptyDistribution.__init__(self,bintype,nbpd,nppb,nlb,nlr,fill_last_decade)
         self.name = name
         self.size = 0
+        self.nbins = 0
         self.vmin = None
         self.vmax = None
         self.ranks = None
@@ -162,12 +162,14 @@ class Distribution(EmptyDistribution):
             np.power(10,-scale_invlog))*100
 
         self.ranks = ranks_invlog
+        self.nbins = self.ranks.size
 
     def getLinRanks(self):
 
         """Percentile ranks regularly spaced on a linear axis of percentile ranks"""
 
         self.ranks = np.linspace(0,100,self.nlr+1)
+        self.nbins = self.ranks.size
 
     def computePercentilesAndBinsFromRanks(self,sample,crop=True):
 
@@ -184,7 +186,7 @@ class Distribution(EmptyDistribution):
 
         sample_no_nan = sample[np.logical_not(np.isnan(sample))]
         if sample_no_nan.size == 0:
-            centers = np.array([np.nan]*self.ranks.size)
+            centers = np.array([np.nan]*self.nbins)
         else:
             centers = np.percentile(sample_no_nan,self.ranks)
 
@@ -192,6 +194,7 @@ class Distribution(EmptyDistribution):
         if crop:
             centers = centers[1:-1]
             self.ranks = self.ranks[1:-1]
+            self.nbins = self.nbins-1
         else:
             temp = breaks.copy()
             breaks = np.array([np.nan]*(temp.size+2))
@@ -250,17 +253,9 @@ class Distribution(EmptyDistribution):
         self.setVminVmax(sample,vmin,vmax,minmode)
         kmin = floor(log10(self.vmin))
         kmax = ceil(log10(self.vmax))
-        # dk = 1/self.nbpd
-        # exps = np.arange(kmin,kmax+dk,dk)
-        # self.bins = np.power(10.,exps)
-        # self.percentiles = np.convolve(self.bins,[0.5,0.5],mode='valid')
-    
         self.bins = np.logspace(kmin,kmax,(kmax-kmin)*self.nbpd)
         self.percentiles = np.convolve(self.bins,[0.5,0.5],mode='valid')
-        # exp_bins = np.arange(kmin,kmax+dk,dk)
-        # exp_centers = np.convolve(exp_bins,[0.5,0.5],mode='valid')
-        # self.bins = np.power(10.,exp_bins)
-        # self.percentiles = np.power(10.,exp_centers)
+        self.nbins = self.percentiles.size
 
     def defineLinearBins(self,sample,vmin=None,vmax=None,minmode='positive'):
 
@@ -276,6 +271,7 @@ class Distribution(EmptyDistribution):
         self.setVminVmax(sample,vmin,vmax,minmode)
         self.bins = np.linspace(self.vmin,self.vmax,self.nlb)
         self.percentiles = np.convolve(self.bins,[0.5,0.5],mode='valid')
+        self.nbins = self.nlb
 
     def computePercentileRanksFromBins(self,sample):
 
@@ -338,17 +334,98 @@ class Distribution(EmptyDistribution):
         density, _ = np.histogram(sample,bins=self.bins,density=True)
         self.density = density
 
+    def indexOfRank(self,rank):
+    
+        """Returns the index of the closest rank in numpy.array ranks"""
+
+        dist_to_rank = np.absolute(np.subtract(self.ranks,rank*np.ones(self.ranks.shape)))
+        mindist = dist_to_rank.min()
+        return np.argmax(dist_to_rank == mindist)
+
+    def rankID(self,rank):
+
+        """Convert rank (float) to rank id (string)
+        """
+
+        return "%2.4f"%rank
+
+    def binIndex(self,percentile=None,rank=None):
+
+        """Returns the index of bin corresponding to percentile or rank 
+        of interest
+        """
+
+        if percentile is not None:
+            # Find first bin edge to be above the percentile of interest
+            i_perc = np.argmax(self.bins > percentile)
+            if i_perc == 0: # Then percentile is outside of stored bins
+                return None
+            return i_perc-1 # Offset by 1
+
+        if rank is not None:
+            return self.indexOfRank(rank)
+        # raise WrongArgument("no percentile or rank is provided in binIndex")
+        return None
+
+    def storeSamplePoints(self,sample,sizemax=50):
+
+        """Find indices of bins in the sample data, to go back and fetch
+        """
+
+        if self.bin_locations_stored:
+            pass
+
+        # Else initalize and find bin locations
+        self.bin_locations = [[] for _ in range(self.nbins)]
+        self.bin_sample_size = [0 for _ in range(self.nbins)]
+
+        # Look at all points, in random order
+        indices = list(range(self.size))
+        random.shuffle(indices)
+
+        for i in indices:
+
+            # Find corresponding bin
+            i_bin = self.binIndex(percentile=sample[i])
+            
+            # Store only if bin was found
+            if i_bin is not None:
+                
+                # Keep count
+                self.bin_sample_size[i] += 1
+                # Store only if there is still room in stored locations list
+                if len(self.bin_locations[i_bin]) < sizemax:
+                    self.bin_locations[i_bin].append(i)
+
+
 
 class ConditionalDistribution():
-"""Class ConditionalDistribution.
+    """Class ConditionalDistribution.
 
-Stores conditional mean and variance in bins of a reference distribution.
-"""
+    Stores conditional mean and variance in bins of a reference distribution.
+    """
 
-    def __init__(self,name='',refDistribution=None):
+    def __init__(self,name='',is3D=False,refDistribution=None):
         """Contructor
+        
+        Arguments:
+        - name
+        - is3D: boolean, if variable is defined in the vertical dimension
+        - refDistribution: Object Distribution containing stats of reference variable
         """
 
         self.name = name
-        self.referenceDistribution = refDistribution
+        self.is3D = is3D
+        self.on = refDistribution
+        self.mean = None
+        self.cond_mean = None
+        self.cond_var = None
         
+    def computeMean(self,sample):
+        pass
+
+    def computeConditionalMean(self,sample):
+        pass
+
+    def computeConditionalVar(self,sample):
+        pass
